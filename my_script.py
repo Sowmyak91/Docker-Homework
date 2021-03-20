b90f5c4f08590384c4f0f129cd8aabeb9f732ccd
Sowmyak91 <78493957+Sowmyak91@users.noreply.github.com>
2021-03-20 02:33:15 -0400

    Task: Added comments in python file.

--- my_script.py
+++ my_script.py
@@ -1,3 +1,5 @@
+# Sample taken from pyStrich GitHub repository
+# https://github.com/mmulqueen/pyStrich
 from pystrich.datamatrix import DataMatrixEncoder
 
 encoder = DataMatrixEncoder('This is a DataMatrix.')
